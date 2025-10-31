67b0f308dcd2ed63ba85a6625b64403e69217f4c
Ralph Ndip <ralphndip02@gmail.com>
2025-10-31 21:53:19 +0000

    8th commit

--- js/index.js
+++ js/index.js
@@ -16,14 +16,18 @@ var app = express();
 app.use(express.json());
 
 
-
+// Enable CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 
 // =====================================
 // MongoDB Local Connection (Database: backendlibrary)
 // =====================================
-var connectionURI = "mongodb://127.0.0.1:27017/backendlibrary";
-var client = new MongoClient(connectionURI);
-var db;
+
 
 async function connectDB() {
   try {
